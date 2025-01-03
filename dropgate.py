e065592ad25dccea2da9085be470b24451413743
JesseFPV <jessehulswit@gmail.com>
2025-01-03 17:18:38 +0100

    Adds code for RPi GPIO

--- dropgate.py
+++ dropgate.py
@@ -1,4 +1,5 @@
 import logging
+import RPi.GPIO as GPIO
 from RHUI import UIField, UIFieldType, UIFieldSelectOption
 
 class Dropgate():
@@ -6,6 +7,8 @@ class Dropgate():
     def __init__(self,rhapi):
         self.logger = logging.getLogger(__name__)
         self._rhapi = rhapi
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
 
     def init_plugin(self,args):
         ui = self._rhapi.ui
@@ -15,11 +18,21 @@ class Dropgate():
         fields.register_option(ui_dropgate_gpio, "fpvscores_dropgate")
         self.logger.info("Dropgate is ready")
 
+    def setup_gpio(self, gpio_pin):
+        GPIO.setup(gpio_pin, GPIO.OUT)
+        GPIO.output(gpio_pin, GPIO.HIGH)
 
     def trigger_drop(self,args):
         fpvscores_dropgate_gpio = self._rhapi.db.option("fpvscores_dropgate_gpio")
-        self.logger.info("Dropping gate on GPIO: %s" % fpvscores_dropgate_gpio)        
+        self.logger.info("Dropping gate on GPIO: %s" % fpvscores_dropgate_gpio)
+        self.setup_gpio(fpvscores_dropgate_gpio)
+        GPIO.output(fpvscores_dropgate_gpio, GPIO.LOW)
 
     def reactivate_drop(self,args):
         fpvscores_dropgate_gpio = self._rhapi.db.option("fpvscores_dropgate_gpio")
         self.logger.info("Reactivating gate on GPIO: %s" % fpvscores_dropgate_gpio)
+        self.setup_gpio(fpvscores_dropgate_gpio)
+        GPIO.output(fpvscores_dropgate_gpio, GPIO.HIGH)
+
+    def gpio_cleanup(self):
+        GPIO.cleanup()
